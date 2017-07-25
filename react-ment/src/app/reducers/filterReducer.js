f5379464f3ac09bfd12b8d79f871c81db5874734
sergei_petukhov@epam.com <sergei_petukhov@epam.com>
2017-07-25 13:13:05 +0300

    filter reducer fixed

--- react-ment/src/app/reducers/filterReducer.js
+++ react-ment/src/app/reducers/filterReducer.js
@@ -14,10 +14,7 @@ const FilterReducer = (state = INITIAL_STATE, action) => {
         };
         case actions.CHECKBOX_UNCHECKED: return {
             ...state,
-            checkboxesChecked: [
-              ...state.checkboxesChecked.slice(0, action.data.optionId),
-              ...state.checkboxesChecked.slice(action.data.optionId + 1)
-            ]};
+            checkboxesChecked: state.checkboxesChecked.filter(elt => elt.optionId !== action.data.optionId);
         case actions.RADIO_ON: return {
             ...state,
             radioOn: [...state.radioOn, action.data.optionId]
